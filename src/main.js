b2e1b914b080736a0f5aeb365179c64ee107b52a
chobrandone <chobrandone4@gmail.com>
2020-11-02 07:44:01 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Dashboard from './views/Dashboard.vue'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 Vue.config.productionTip = false
 
+const routes = {
+  '/dashboard': Dashboard
+}
 new Vue({
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || Dashboard
+    }
+  },
   render: h => h(App),
 }).$mount('#app')
